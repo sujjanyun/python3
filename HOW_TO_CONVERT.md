ae9d182495a96f4ea8937c24cc83176ad6071723
Susan Yun <susanhkyun@gmail.com>
2021-02-08 14:34:50 -0600

    updating

---
+++ HOW_TO_CONVERT.md
@@ -0,0 +1,18 @@
+# How to convert our existing code into OOP
+* Create a class (blueprint that defines what they all are), maybe call it 'Pets'
+
+# AT LEAST 3 METHODS
+* 'feed pet'
+* 'play with pet'
+* 'get hungry and mopey*
+
+# TWO CLASS 2 ATTRIBUTES (maybe more/maybe instance)
+* 'fullness'
+* 'happiness'
+
+# 1 INSTANCE ATTRIBUTE
+* 'name'
+
+# function that needed - self always shows up
+def __init__(self, name)
+# maybe more
--- cats.py
+++ cats.py
@@ -1,2 +1,23 @@
-cat Cat:
+class Cat:
+# CLASS ATTRIBUTE - applies to all cats!
     species = "Mammal"
+
+# INSTANCES ATTRIBUTES - different for each instance of cat
+# self means it is pointing back to itself (the class of cat)
+    def __init__(self, name, age):
+        super().__init__()
+        self.name = name
+        self.age = age
+    
+# INSTANCE METHOD - different for each instance of cat    
+    def description(self):
+        return "{} is {} years old.".format(self.name, self.age)
+
+gus = Cat("Gus", 10)
+beans = Cat("Beans", 11)
+print(gus.name)
+print(gus.age)
+print(gus.species)
+
+print(gus.description())
+print(beans.description())
